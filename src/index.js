4a7c7b2745b31e01639d66c1c54cda64808ee368
Richard Rowe <richard.r@everpayinc.com>
2019-01-10 01:56:45 -0600

    6th commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// JS Goes here - ES6 supported
+import "https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900";
+import "https://fonts.googleapis.com/css?family=Barlow:300,400,500,600,700";
+import "./vendor/js/material-ripple.js";
+import "./vendor/js/layout-helpers.js";
+import "./vendor/js/pace.js";
+import "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+import "./vendor/libs/popper/popper.js";
+import "./vendor/js/bootstrap.js";
+import "./vendor/libs/swiper/swiper.js";
+import "./vendor/libs/plyr/plyr.js";
+
+import "./vendor/fonts/ionicons.css";
+import "./vendor/fonts/linearicons.css";
+import "./css/bootstrap.css";
+import "./css/base.css";
+import "./css/theme.css";
+import "./css/colors.css";
+import "./css/ui.css";
+import "./css/landing.css";
+
+import "./vendor/libs/swiper/swiper.css";
+import "./vendor/libs/plyr/plyr.css";
+// Say hello
+console.log("🦊 Hello! Edit me in src/index.js");
